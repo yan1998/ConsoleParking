046f83166214616716ac2a9f334c13d7d7ffb521
Yan Gorshkov <gorshkov.yan1998@gmail.com>
2018-05-11 15:54:30 +0300

    Added filed and properties for class Car

--- ConsoleParking/Car.cs
+++ ConsoleParking/Car.cs
@@ -4,7 +4,41 @@ using System.Text;
 
 namespace ConsoleParking
 {
-    class Car
+    public class Car
     {
+        private double balance;
+
+        public Car(string carNumber, double balance, CarType carType)
+        {
+            CarNumber = carNumber;
+            Balance = balance;
+            CarType = carType;
+        }
+
+        /// <summary>
+        /// Properties for set car identifier
+        /// </summary>
+        public string CarNumber{ get; }
+
+        /// <summary>
+        /// Properties for car balance
+        /// </summary>
+        public double Balance
+        {
+            get { return balance; }
+            set
+            {
+                if (value > 0)
+                    balance = value;
+                else
+                    throw new Exception("Баланс не может быть равным нулю");
+            }
+        }
+
+        /// <summary>
+        /// Properties for get car type
+        /// </summary>
+        public CarType CarType { get; }
+
     }
 }
