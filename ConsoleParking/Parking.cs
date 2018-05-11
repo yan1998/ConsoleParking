4a6636a6a5e121960637628382dae126a5b57da3
Yan Gorshkov <gorshkov.yan1998@gmail.com>
2018-05-11 15:41:17 +0300

    Added filed and properties for Parking class

--- ConsoleParking/Parking.cs
+++ ConsoleParking/Parking.cs
@@ -4,7 +4,53 @@ using System.Text;
 
 namespace ConsoleParking
 {
-    class Parking
+    public class Parking
     {
+        /// <summary>
+        /// Instance of singleton object
+        /// </summary>
+        private static Lazy<Parking> parking { get; set; } = new Lazy<Parking>(() => new Parking());
+
+        /// <summary>
+        /// Property for get singleton object
+        /// </summary>
+        public static Parking Instance
+        {
+            get { return parking.Value; }        
+        }
+
+        private double balance=0;
+
+        private Parking() { }
+
+        /// <summary>
+        /// Get parking settings 
+        /// </summary>
+        public Settings Settings{ get { return Settings.Instance; } }
+
+        /// <summary>
+        /// Get list of cars which staying in parking
+        /// </summary>
+        public List<Car> Cars { get; } = new List<Car>();
+
+        /// <summary>
+        /// Get list of transactions
+        /// </summary>
+        public List<Transaction> Transactions { get; } = new List<Transaction>();
+
+        /// <summary>
+        /// Show parking income
+        /// </summary>
+        public double Balance
+        {
+            get { return parking.Value.balance; }
+            set
+            {
+                if (value > 0)
+                    parking.Value.balance = value;
+                else
+                    throw new Exception("Баланс не может быть меньше нуля!");
+            }
+        }
     }
 }
